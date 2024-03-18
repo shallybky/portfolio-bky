4ed2136741a95df97fe46a0bfe3d5650cb98c0be
bakayoko <bakayoko@gmail.com>
2024-03-18 17:43:29 +0100

    ajout de l'animation sur le bouton envoyer

--- script.js
+++ script.js
@@ -2,3 +2,21 @@ function toggleMenu() {
     var nav = document.querySelector('nav');
     nav.classList.toggle('open');
 }
+
+function afficherMessage() {
+    var messageDiv = document.createElement("div");
+    messageDiv.textContent = "Message envoyé";
+    messageDiv.classList.add("message-envoye");
+
+    document.body.appendChild(messageDiv);
+
+    setTimeout(function() {
+        messageDiv.classList.add('hide');
+    }, 0); 
+}
+
+document.querySelector("form").addEventListener("submit", function(event) {
+    event.preventDefault(); 
+    afficherMessage();
+    this.submit(); 
+});
