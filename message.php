e6ab06076fb91e32bec4410e5941323c8cefcb70
bakayoko <bakayoko@gmail.com>
2024-03-18 19:56:06 +0100

    installation de phpmailer

--- message.php
+++
@@ -1,5 +0,0 @@
-<?php
-    $retour = mail('bakayoko.shally.b@gmail.com', 'Envoi depuis la page Contact', $_POST['message'], 'From: https://shallybky.github.io/portfolio-bky/');
-    if ($retour)
-        echo '<p>Votre message a bien été envoyé.</p>';
-    ?>
