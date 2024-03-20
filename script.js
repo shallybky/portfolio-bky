d8d8bdbfc4a9cee58d982c29df9b5ac3adabe080
bakayoko <bakayoko@gmail.com>
2024-03-20 16:44:31 +0100

    ajout de l'animation de la bar

--- script.js
+++ script.js
@@ -9,3 +9,17 @@ document.getElementById("submitBtn").addEventListener("click", function() {
         document.getElementById("messageSent").classList.add("hide");
     }, 5000 ); 
 });
+
+document.addEventListener('scroll', function() {
+    var competencesSection = document.getElementById('competences');
+    var windowHeight = window.innerHeight;
+    var scrollY = window.scrollY || window.pageYOffset;
+
+    var competencesSectionTop = competencesSection.getBoundingClientRect().top + scrollY;
+
+    if (scrollY > competencesSectionTop - windowHeight + 200) {
+        competencesSection.classList.add('animate');
+    }
+});
+
+
