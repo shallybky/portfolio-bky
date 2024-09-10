2f82e21f9f242c893e0890c5b9b5ca128afbfb60
bakayoko <bakayoko@gmail.com>
2024-09-10 13:06:55 +0200

    mise à jour

--- script.js
+++ script.js
@@ -1,38 +1,6 @@
-document.addEventListener("DOMContentLoaded", function () {
-    let observer = new IntersectionObserver((entries, observer) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add("visible");
-                observer.unobserve(entry.target);
-            }
-        });
-    }, { threshold: 0.1 });
+const burgerMenu = document.querySelector('.burger-menu');
+const navMenu = document.querySelector('header nav ul');
 
-    document.querySelectorAll(".about .quality").forEach(quality => {
-        observer.observe(quality);
-    });
+burgerMenu.addEventListener('click', () => {
+    navMenu.classList.toggle('show');
 });
-
-document.addEventListener("DOMContentLoaded", function() {
-    const menuLinks = document.querySelectorAll('.menu a');
-
-    menuLinks.forEach(link => {
-        link.addEventListener('click', function(e) {
-            e.preventDefault(); 
-
-            const targetId = this.getAttribute('href').substring(1);
-            const targetElement = document.getElementById(targetId);
-
-            if (targetElement) {
-                targetElement.scrollIntoView({
-                    behavior: 'smooth'
-                });
-            }
-        });
-    });
-});
-
-function toggleMenu() {
-    const menu = document.querySelector('.menu');
-    menu.classList.toggle('active');
-}
